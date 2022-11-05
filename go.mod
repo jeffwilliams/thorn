c21b0716044a537eaa590222f4c3cb3575c3916c
Jeff Williams <kanobe@gmail.com>
2022-11-04 21:53:22 -0400

    Code

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/jeffwilliams/thorn
+
+go 1.18
---
+++ main.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"os"
+	"strings"
+)
+
+func main() {
+	b, err := ioutil.ReadAll(os.Stdin)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "read err: %v\n", err)
+		os.Exit(1)
+	}
+
+	s := string(b)
+
+	s = strings.ReplaceAll(s, "th", "þ")
+	s = strings.ReplaceAll(s, "Th", "Þ")
+	s = strings.ReplaceAll(s, "and", "⁊")
+	s = strings.ReplaceAll(s, "And", "⁊")
+	s = strings.ReplaceAll(s, "ss", "ß")
+
+	fmt.Printf(s)
+}
